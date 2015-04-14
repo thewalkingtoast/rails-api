fa1d1c36c8c769d6902aafcedf22eed0cf03b974
Santiago Pastorino <santiago@wyeworks.com>
2015-04-14 11:19:27 -0300

    Use the new get syntax to avoid warning in controller tests

--- test/api_controller/renderers_test.rb
+++ test/api_controller/renderers_test.rb
@@ -49,21 +49,21 @@ class RenderExceptionsTest < ActionDispatch::IntegrationTest
   end
 
   def test_render_json_exception
-    get "/fake", {}, 'HTTP_ACCEPT' => 'application/json'
+    get "/fake", headers: { 'HTTP_ACCEPT' => 'application/json' }
     assert_response :internal_server_error
     assert_equal 'application/json', response.content_type.to_s
     assert_equal({ :status => '500', :error => 'boom' }.to_json, response.body)
   end
 
   def test_render_xml_exception
-    get "/fake", {}, 'HTTP_ACCEPT' => 'application/xml'
+    get "/fake", headers: { 'HTTP_ACCEPT' => 'application/xml' }
     assert_response :internal_server_error
     assert_equal 'application/xml', response.content_type.to_s
     assert_equal({ :status => '500', :error => 'boom' }.to_xml, response.body)
   end
 
   def test_render_fallback_exception
-    get "/fake", {}, 'HTTP_ACCEPT' => 'text/csv'
+    get "/fake", headers: { 'HTTP_ACCEPT' => 'text/csv' }
     assert_response :internal_server_error
     assert_equal 'text/html', response.content_type.to_s
   end
