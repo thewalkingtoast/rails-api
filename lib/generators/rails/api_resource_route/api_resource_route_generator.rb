01a5fda56befecfccd0d293a048b247bf3eeb0dc
Santiago Pastorino <santiago@wyeworks.com>
2015-04-15 13:50:41 -0300

    Resync api_resource_route with Rails master's resource_route

--- lib/generators/rails/api_resource_route/api_resource_route_generator.rb
+++ lib/generators/rails/api_resource_route/api_resource_route_generator.rb
@@ -2,14 +2,27 @@ require 'rails/generators/rails/resource_route/resource_route_generator'
 
 module Rails
   module Generators
-    class ApiResourceRouteGenerator < ResourceRouteGenerator
+    class ApiResourceRouteGenerator < ResourceRouteGenerator # :nodoc:
       def add_resource_route
         return if options[:actions].present?
-        route_config =  regular_class_path.collect{ |namespace| "namespace :#{namespace} do " }.join(" ")
-        route_config << "resources :#{file_name.pluralize}"
-        route_config << ", except: [:new, :edit]"
-        route_config << " end" * regular_class_path.size
-        route route_config
+
+        # iterates over all namespaces and opens up blocks
+        regular_class_path.each_with_index do |namespace, index|
+          write("namespace :#{namespace} do", index + 1)
+        end
+
+        # inserts the primary resource
+        write("resources :#{file_name.pluralize}, except: [:new, :edit]", route_length + 1)
+
+        # ends blocks
+        regular_class_path.each_index do |index|
+          write("end", route_length - index)
+        end
+
+        # route prepends two spaces onto the front of the string that is passed, this corrects that.
+        # Also it adds a \n to the end of each line, as route already adds that
+        # we need to correct that too.
+        route route_string[2..-2]
       end
     end
   end
